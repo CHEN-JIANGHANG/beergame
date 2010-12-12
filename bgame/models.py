22175ea7dfef175523dd3229eca91acd7bf1ebad
Taylor McKay <taylormckay@gmail.com>
2010-12-11 20:39:08 -0800

    modified models for stash, started sessions

--- bgame/models.py
+++ bgame/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 
 class Game(models.Model):
-    date_started = models.DateTimeField(auto_now_add=True)
+    started = models.DateTimeField(auto_now_add=True)
+    ended = models.DateTimeField(null=True)
     name = models.CharField(max_length=40, unique=True)
     make_available = models.BooleanField(default=True)
+    notes = models.TextField(blank=True, null=True)
 
     def __unicode__(self):
         return self.name
 
 class Team(models.Model):
-    ROLE_CHOICES = (
+    ROLES = (
                         ('Factory','Factory'),
                         ('Distributor','Distributor'),
                         ('Wholesaler','Wholesaler'),
@@ -17,19 +19,21 @@ class Team(models.Model):
                     )
 
     BUTTONS = (
+                    ('none','none'),
                     ('start','start'),
                     ('step1','step1'),
                     ('step2','step2'),
                     ('ship','ship'),
                     ('step3','step3'),
                     ('order','order'),
-                    ('none','none')
                 )
 
     game = models.ForeignKey(Game)
-    role = models.CharField(max_length=12, choices=ROLE_CHOICES)
+    role = models.CharField(max_length=12, choices=ROLES)
     last_completed_period = models.IntegerField(default=0)
     last_clicked_button = models.CharField(max_length=12, choices=BUTTONS, default="none") 
+
+    session = models.CharField(max_length=40, blank=True, null=True)
 
     def save(self, *args, **kwargs):
         is_new = self.pk is None
@@ -46,19 +50,31 @@ class Team(models.Model):
         return "%s playing in %s" % (self.role, self.game.name)
 
 class Period(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+
     team = models.ForeignKey(Team)
     number = models.IntegerField(default=0)
+
     inventory = models.IntegerField(default=12)
     backlog = models.IntegerField(default=0)
+
     demand = models.IntegerField(blank=True, null=True)
     order_1 = models.IntegerField(blank=True, null=True, default=4)
     order_2 = models.IntegerField(blank=True, null=True, default=4)
+
+    # stores orders when team has not advanced order_1 => demand 
+    # and order_2 => order_1
+    order_stash = models.IntegerField(blank=True, null=True) 
+
     shipment_1 = models.IntegerField(blank=True, null=True, default=4)
     shipment_2 = models.IntegerField(blank=True, null=True, default=4)
+    shipment_stash = models.IntegerField(blank=True, null=True)
+
     shipped = models.IntegerField(blank=True, null=True)
+    order = models.IntegerField(blank=True, null=True, default=0)
+
     cost = models.DecimalField(max_digits=8, decimal_places=2, default='0.00')
     cumulative_cost = models.DecimalField(max_digits=8, decimal_places=2, default='0.00')
-    order = models.IntegerField(blank=True, null=True, default=0)
 
     def __unicode__(self):
         return "%d / %s / %s" % (self.number, self.team.role, self.team.game.name)
